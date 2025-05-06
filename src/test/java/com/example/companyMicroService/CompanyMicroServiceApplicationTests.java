d42b1973b97a5301b449422fff9af5edfac07c98
Deeksha Ch <deekshach252@gmail.com>
2025-05-06 15:27:34 -0500

    Company Microservice

---
+++ src/test/java/com/example/companyMicroService/CompanyMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companyMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
